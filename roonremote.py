685855ab5c50035b6ee78c7ee6a1dbdedc7b89d7
root <Matt Eckhaus>
2018-05-12 22:47:02 +0100

    Remote uses harmony commands for volume and mute

--- roonremote.py
+++ roonremote.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+
+#TODO: find out default zone from roonrest and set volume/mute accordingly
+
 import evdev
 import select
 import requests
+import subprocess
 
-BASE_URL = "http://localhost:3000/api/v1"
-ZONE = "current" # Use "current" for zone selected in Roon settings
 
+roon_base_url = "http://roonstation:3000/api/v1"
+harmony_base_url = "http://m1:8282/hubs/harmony-hub/devices/channel-islands-audio-amp/commands"
+
+myzone = "default"
 devices = {}
 for fn in evdev.list_devices():
+    print(fn)
     dev = evdev.InputDevice(fn)
     if dev.name.find('HBGIC') >= 0:
         devices[dev.fd] = dev
@@ -17,6 +24,7 @@ while True:
     for fd in r:
         for event in devices[fd].read():
             url = None
+            cmd = None
             if event.type == evdev.ecodes.EV_KEY:
                 keyev = evdev.categorize(event)
                 code = keyev.keycode[4:]
@@ -28,27 +36,36 @@ while True:
                 elif state == evdev.events.KeyEvent.key_hold:
                     state = "HOLD"
                 print(code, state)
+                method = "GET"
                 if state == "DOWN":
                     if code == "PLAYPAUSE":
-                        url = "%s/zone/current/control/playpause" % BASE_URL
+                        url = "%s/zone/%s/control/playpause" % (roon_base_url, myzone)
                     elif code == "STOP":
-                        url = "%s/zone/all/control/pause" % BASE_URL
+                        url = "%s/zone/all/control/pause" % roon_base_url
+                        #url = "%s/zone/%s/control/stop" % (base_url, myzone)
                     elif code == "REWIND":
-                        url = "%s/zone/current/control/previous" % BASE_URL
+                        url = "%s/zone/%s/control/previous" % (roon_base_url, myzone)
                     elif code == "FASTFORWARD":
-                        url = "%s/zone/current/control/next" % BASE_URL
-                    elif code == "UP":
-                        url = "%s/zone/current/volume/relative_step/1" % BASE_URL
-                    elif code == "DOWN":
-                        url = "%s/zone/current/volume/relative_step/-1" % BASE_URL
-                if state == "HOLD":
+                        url = "%s/zone/%s/control/next" % (roon_base_url, myzone)
+                    elif code == "INFO":
+                        method = "POST"
+                        url = "%s/mute" % harmony_base_url
+                if state == "HOLD" or state == "DOWN":
                     if code == "UP":
-                        url = "%s/zone/current/volume/relative_step/1" % BASE_URL
+                        method = "POST"
+                        url = "%s/volume-up" % harmony_base_url
                     elif code == "DOWN":
-                        url = "%s/zone/current/volume/relative_step/-1" % BASE_URL
+                        method = "POST"
+                        url = "%s/volume-down" % harmony_base_url
             if url:
-                print(url)
+                print(method, url)
                 try:
-                    req = requests.put(url)
+                    if method == "GET":
+                        req = requests.get(url)
+                    else:
+                        req = requests.post(url)
                 except:
                     print("Request to %s failed" % (url))
+            if cmd:
+                print(" ".join(cmd))
+                subprocess.call(cmd)
